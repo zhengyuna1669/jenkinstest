b84c0d32b7f94ba5ff9169752105d8f21dfa98df
zhengyn1114 <zhengyn1114@163.com>
2019-01-24 16:37:53 +0800

    jenkinstest01

---
+++ scripts/test_02_login.py
@@ -0,0 +1,17 @@
+import allure
+
+
+def test_a():
+    print("aaa")
+
+@allure.step(title="测试登录的流程")
+def test_login():
+    allure.attach('登录','输入用户名')
+    print("aaa")
+
+    allure.attach('登录','输入密码')
+    print("bbb")
+
+    allure.attach('登录','点击登录')
+    print("ccc")
+    assert 0
---
+++ scripts/test_all_report.py
@@ -0,0 +1,8 @@
+class Test01:
+    def test_a(self):
+        print("aaa")
+        assert 1
+
+    def test_b(self):
+        print("bbb")
+        assert 0
